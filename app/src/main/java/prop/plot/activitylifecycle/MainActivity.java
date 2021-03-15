97e05d5e78b6e206cde21fd5a2982e36d5613e26
bhakadekailas@belebentech.com <bhakadekailas@belebentech.com>
2021-03-15 18:07:40 +0530

    Activity Life cycle example added with dialog

---
+++ app/src/main/java/prop/plot/activitylifecycle/MainActivity.java
@@ -0,0 +1,66 @@
+package prop.plot.activitylifecycle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate: ");
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy: ");
+    }
+
+    public void callDialog(View view) {
+        Log.e(TAG, "callDialog: ");
+        new AlertDialog.Builder(this)
+                .setTitle("My Dialog")
+                .setMessage("This is my dialog, How Are You!!")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+}
